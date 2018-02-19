8b7da96ea9f1bff8a67f3347f161360f31de7f37
Amar Chatman <amarjerome@gmail.com>
2018-02-19 03:35:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,8 +7,7 @@ end
 
 def reverse_each_word(string)
 array_new = []
-string.split.collect do |word| array_new << word.reverse
-  array_new.join
+string.split.collect do |word| array_new.join << word.reverse
 end
 return array_new
 end
