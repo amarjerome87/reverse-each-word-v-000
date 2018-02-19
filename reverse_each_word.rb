3f25cc364df532c1ad8cb8feaa321b190c35aae7
Amar Chatman <amarjerome@gmail.com>
2018-02-19 03:26:21 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+require "pry"
+def reverse_each_word(string)
+new_array = []
+string.split.each  {|word|new_array << word.reverse}
+return new_array.join( " " )
+end
+
+def reverse_each_word(string)
+array_new = []
+array_new.collect do |word| word.reverse
+end
+end 
